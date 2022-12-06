454f58287174976ce099453c27a997c18e58606a
khoubaib <khoubaib15@outlook.com>
2022-12-06 21:16:14 +0000

    added questions

---
+++ assets/js/questions.js
@@ -0,0 +1,31 @@
+var questions = [
+    {
+        title: "Question no.1: '...'? ",
+        choices: ['Choice 1', 'Choice 2', 'Choice 3', 'Choice 4'],
+        answer: "Choice 3"
+    },
+    {
+        title: "Question no.2: '...'? ",
+        choices: ['Choice 1', 'Choice 2', 'Choice 3', 'Choice 4'],
+        answer: "Choice 2"
+    }, {
+        title: "Question no.3: '...'? ",
+        choices: ['Choice 1', 'Choice 2', 'Choice 3', 'Choice 4'],
+        answer: "Choice 4"
+    },
+    {
+        title: "Question no.4: '...'? ",
+        choices: ['Choice 1', 'Choice 2', 'Choice 3', 'Choice 4'],
+        answer: "Choice 4"
+    },
+    {
+        title: "Question no.5: '...'? ",
+        choices: ['Choice 1', 'Choice 2', 'Choice 3', 'Choice 4'],
+        answer: "Choice 1"
+    },
+    {
+        title: "Question no.6: '...'? ",
+        choices: ['Choice 1', 'Choice 2', 'Choice 3', 'Choice 4'],
+        answer: "Choice 3"
+    },
+]
