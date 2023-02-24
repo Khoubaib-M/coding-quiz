5fe1c050ad945fbcb7abbde91c60840441125d49
khoubaib <khoubaib15@outlook.com>
2023-02-24 15:03:41 +0000

    fixed some errors

--- assets/js/questions.js
+++ assets/js/questions.js
@@ -1,30 +1,30 @@
 var questions = [
     {
-        title: "Question no.1: How to stop an interval timer in Javascript? ",
+        question: "Question no.1: How to stop an interval timer in Javascript? ",
         choices: ['clearInterval', 'intervalOver', 'clearTimer', 'None of the above'],
         answer: "clearInterval"
     },
     {
-        title: "Question no.2: What are JavaScript Data Types? ",
+        question: "Question no.2: What are JavaScript Data Types? ",
         choices: ['Number', 'Boolean', 'String', 'All of the above'],
         answer: "All of the above"
     }, {
-        title: "Question no.3: How do you add a comment to JavaScript code? ",
+        question: "Question no.3: How do you add a comment to JavaScript code? ",
         choices: ['! comment', '*/ comment', '// comment', '# comment'],
         answer: "// comment"
     },
     {
-        title: "Question no.4: The condition in an if/else statement is enclosed with ______. ",
+        question: "Question no.4: The condition in an if/else statement is enclosed with ______. ",
         choices: ['square brackets', 'parenthesis', 'curly brackets', 'quotes'],
         answer: "parenthesis"
     },
     {
-        title: "Question no.5: Which value is not falsey? ",
+        question: "Question no.5: Which value is not falsey? ",
         choices: ['null', 'undefined', '0', '[ ]'],
         answer: "[ ]"
     },
     {
-        title: "Question no.6: What statement can be used to skip an iteration in a loop ",
+        question: "Question no.6: What statement can be used to skip an iteration in a loop? ",
         choices: ['skip', 'continue', 'pass', 'break'],
         answer: "continue"
     },
