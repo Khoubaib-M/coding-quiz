f5466b004a05142533bf6ff6eacf2af6242c70fc
khoubaib <khoubaib15@outlook.com>
2023-02-24 14:59:15 +0000

    added questions

--- assets/js/questions.js
+++ assets/js/questions.js
@@ -1,31 +1,31 @@
 var questions = [
     {
-        title: "Question no.1: '...'? ",
-        choices: ['Choice 1', 'Choice 2', 'Choice 3', 'Choice 4'],
-        answer: "Choice 3"
+        title: "Question no.1: How to stop an interval timer in Javascript? ",
+        choices: ['clearInterval', 'intervalOver', 'clearTimer', 'None of the above'],
+        answer: "clearInterval"
     },
     {
-        title: "Question no.2: '...'? ",
-        choices: ['Choice 1', 'Choice 2', 'Choice 3', 'Choice 4'],
-        answer: "Choice 2"
+        title: "Question no.2: What are JavaScript Data Types? ",
+        choices: ['Number', 'Boolean', 'String', 'All of the above'],
+        answer: "All of the above"
     }, {
-        title: "Question no.3: '...'? ",
-        choices: ['Choice 1', 'Choice 2', 'Choice 3', 'Choice 4'],
-        answer: "Choice 4"
+        title: "Question no.3: How do you add a comment to JavaScript code? ",
+        choices: ['! comment', '*/ comment', '// comment', '# comment'],
+        answer: "// comment"
     },
     {
-        title: "Question no.4: '...'? ",
-        choices: ['Choice 1', 'Choice 2', 'Choice 3', 'Choice 4'],
-        answer: "Choice 4"
+        title: "Question no.4: The condition in an if/else statement is enclosed with ______. ",
+        choices: ['square brackets', 'parenthesis', 'curly brackets', 'quotes'],
+        answer: "parenthesis"
     },
     {
-        title: "Question no.5: '...'? ",
-        choices: ['Choice 1', 'Choice 2', 'Choice 3', 'Choice 4'],
-        answer: "Choice 1"
+        title: "Question no.5: Which value is not falsey? ",
+        choices: ['null', 'undefined', '0', '[ ]'],
+        answer: "[ ]"
     },
     {
-        title: "Question no.6: '...'? ",
-        choices: ['Choice 1', 'Choice 2', 'Choice 3', 'Choice 4'],
-        answer: "Choice 3"
+        title: "Question no.6: What statement can be used to skip an iteration in a loop ",
+        choices: ['skip', 'continue', 'pass', 'break'],
+        answer: "continue"
     },
 ]
